2d270206146b551a2595b1621d8480d8694bf406
kion <kionkramer@hetnet.nl>
2026-07-13 19:31:50 +0200

    nieuwe tests

--- train_iclnoise.py
+++ train_iclnoise.py
@@ -60,7 +60,7 @@ import matplotlib.pyplot as plt
 
 
 
-EXPERIMENT_NAME = "13juli_CONTEXTSIZE16.2_ICL_general"
+EXPERIMENT_NAME = "13juli_CONTEXTSIZE16.3_ICL_general"
 BASE_DATA_DIR = Path(os.environ["DATA_DIR"])
 
 class SoftDiceLoss(nn.Module):
