6dffe3c85e7c4b5192e4c71570db1b0cfca424dc
kion <kionkramer@hetnet.nl>
2026-07-13 19:16:42 +0200

    nieuwe tests

--- train_iclnoise.py
+++ train_iclnoise.py
@@ -684,7 +684,7 @@ train_context = X.copy()
 # Belangrijk:
 # Voor HE-test kun je train_context gebruiken.
 # Voor cross-stain test is Y.copy() logisch als je same-stain context wil gebruiken.
-test_context = X.copy()
+test_context = []
 
 
 
